5196c7c614821edb3d7f3cf5ede3e6cb7ae94290
spata00 <sararares12@gmail.com>
2021-10-27 15:18:06 +0200

    collegamenti fatti

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $arrayelement = config('comics');
+    $data=[
+        'comics'=>$arrayelement,
+    ];
+    return view('home',$data);
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 
-mix.option({processCssUrls: false})
+mix.options({processCssUrls: false})
